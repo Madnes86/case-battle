8c038e6deb4a705802a4cd48ddf6bfd04791303e
Vadim <tolkott35@gmail.com>
2025-07-03 08:54:51 +0500

    init

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    
+    const SLOGAN = "У нас выйгрывают";
+</script>
+
+<header>
+    <a href="/" class="fixed top-0 left-0 w-[190px] h-[68px]">
+        <img src="/img/logo.svg" alt="{SLOGAN}" class="">
+        <p class="text-[10px]">{SLOGAN}</p>
+    </a>
+</header>
