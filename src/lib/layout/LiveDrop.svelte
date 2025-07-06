d66f7f8b004e4fac9cd9a653f256cb0029df3919
Vadim <tolkott35@gmail.com>
2025-07-06 19:57:33 +0500

    Update LiveDrop

--- src/lib/layout/LiveDrop.svelte
+++ src/lib/layout/LiveDrop.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-    import { drops } from '$lib/data/drops'; // re name
+    import { DROPS } from '$lib/data/drops'; // re name
 	import { onMount } from 'svelte';
 
     const DROPS_MAX : number = 8;
     let hover : number | null = $state(null);
-    let thisDrops = $state(drops); // re name 
+    let drops = $state(DROPS); // re name 
 
     function onEnter(index : number) { hover = index };
     function onLeave() { hover = null };
 
     function addDrop() {
         if (hover != null) return;
-        if (thisDrops.length >= 8) { thisDrops.pop() };
-        const RANDOM = Math.floor(Math.random() * drops.length);
-        thisDrops = [drops[RANDOM], ...thisDrops];
+        if (drops.length >= 8) { drops.pop() };
+        const RANDOM = Math.floor(Math.random() * DROPS.length);
+        drops = [DROPS[RANDOM], ...drops];
     };
     function getDrop() {
         setInterval(addDrop, 1_000);
@@ -23,22 +23,22 @@
 </script>
 
 <aside class="fixed left-0 top-[68px] w-[190px] h-[calc(100vh-68px)] flex flex-col gap-1">
-    {#each thisDrops as { rarity, itemImg, itemName, fromName, fromLink, fromImg, steamLink, steamImg }, index }
+    {#each drops as { rarity, itemImg, itemName, fromName, fromLink, fromImg, steamLink, steamImg }, index }
     <div 
     onmouseenter="{() => {onEnter(index)}}" 
     onmouseleave="{onLeave}" 
     role="list" 
     style="{hover == index 
         ? `width: 250px; background: ${rarity}; border-color: ${rarity}` 
-        : `border-color: ${rarity}`}"
-    class="flex overflow-hidden border-l-4 h-[100px]">
+        : `border-color: ${rarity}; background: ${rarity.replace('rgb', 'rgba').replace(')', ', 0.6)')}`}"
+    class="flex overflow-hidden border-l-4 h-[100px] text-[12px]">
         <div class="p-2 w-[190px] flex flex-col gap-2 justify-center items-center text-white">
             {#if hover != index }
-            <img src="{itemImg}" alt="test" class="max-w-full max-h-full object-contain">
-            <p>{itemName}</p>
+            <img src="{itemImg}" alt="test" class="h-[70%] object-contain">
+            <p class="truncate whitespace-nowrap w-full text-center">{itemName}</p>
             {:else}
-            <p>{fromName}</p>
-            <a href="{fromLink}" class="max-w-full h-[50%]"><img src="{fromImg}" alt="test" class="max-w-full max-h-full object-contain"></a>
+            <p class="uppercase truncate whitespace-nowrap w-full text-center">{fromName}</p>
+            <a href="{fromLink}" class="h-[70%]"><img src="{fromImg}" alt="test" class="h-full object-contain"></a>
             {/if}
         </div>
         {#if hover == index }
