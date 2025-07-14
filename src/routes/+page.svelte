d417bd9fc64066751e9fabea90a6de38e0e922f2
Vadim <tolkott35@gmail.com>
2025-07-14 11:11:31 +0500

    fix: show cats & filter cats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
     import { CASES } from '$lib/data/cases';
     import { Case } from '$lib/components';
+    import { user } from '../stores/user';
 
     // Search
     const PLACEHOLDER : string = "Что ищем?";
     let cases = CASES;
     let windowWidth : number = $state(0);
-    let search : string = $state('');
-    let show : boolean = $derived(windowWidth > 800);
-    let selectedCategory : string | null = $state(null);
-    let filteredCases = $derived(filterCases(cases, search, selectedCategory));
-    let categories = Array.from(new Set(cases.map(c => c.category)));
+    let search      : string = $state('');
+    let show        : boolean = $derived(windowWidth > 800);   
+    let selCats : string[] = $derived([]);
+    let showCases : typeof CASES = $derived(filterCases(cases, search));
+    let cats = Array.from(new Set(cases.map(c => c.category)));
+    let showCats = $derived(selCats.length == 0 ? Array.from(new Set(showCases.map(c => c.category))) : selCats);
 
-    function searchView() { 
-        show = !show 
-    };
-    function filterCases(cases: typeof CASES, query: string, category?: string | null): typeof CASES {
+    function searchView() { show = !show };
+    function filterCases(cases: typeof CASES, query: string): typeof CASES {
         const q = query.toString().toLowerCase();
         return cases.filter(c => {
             const matchQuery = 
@@ -23,30 +23,33 @@
                 c.price.toString().includes(q)   ||
                 c.category.toLowerCase().includes(q);
             
-            const matchCategory = category ? c.category === category : true;
-            return matchQuery && matchCategory;
+            return matchQuery
         });
     };
-
-    // перевод в uppercase categiry
-
+    function addCategory(category: string) {
+        if (!selCats.includes(category)) {
+            selCats = [...selCats, category];
+        } else {
+            selCats = selCats.filter(c => c !== category);
+        }
+    };
 
 </script>   <svelte:window bind:innerWidth={windowWidth} />
 
     <nav class="flex gap-2 justify-center items-center p-2 h-[60px] bg-[var(--color-filter)] text-[var(--color-gray)]">
         <button onclick={searchView} class="hover:opacity-50"><img src="/icon/search.svg" alt=""></button>
         {#if show }<input bind:value={search} placeholder={PLACEHOLDER} type="text" class="w-[200px] h-[30px] bg-[var(--color-header)] text-white">{/if}
-        <button class="hover:opacity-50"><img src="/icon/like.svg" alt=""></button>
-        <button onclick={() => {selectedCategory = null}} class="{selectedCategory != null && 'text-[var(--color-accent)]'} hover:opacity-50"><img src="/icon/clear.svg" alt=""></button>
-        {#each categories as category }
-            <button onclick={() => {selectedCategory = category}} class="{selectedCategory == category && 'text-[var(--color-accent)]'} text-[15px] hover:opacity-50">{category}</button>
+        {#if $user }<button class="hover:opacity-50"><img src="/icon/like.svg" alt=""></button>{/if}
+        {#if selCats.length > 0 }<button onclick={() => {selCats = []}} class="{selCats != null && 'text-[var(--color-accent)]'} hover:opacity-50"><img src="/icon/clear.svg" alt=""></button>{/if} 
+        {#each cats as cat }
+            <button onclick={() => {addCategory(cat)}} class="{selCats.includes(cat) && 'text-[var(--color-accent)]'} text-[15px] hover:opacity-50">{cat}</button>
         {/each}
     </nav>
     <div class="flex flex-col items-center">
-    {#each categories as category }
-        <b class="w-full h-[87px] flex justify-center items-center uppercase text-[15px] bg-center bg-no-repeat bg-[url(/img/case/category.png)] text-white">{category}</b>
+    {#each showCats as cat }
+        <b class="w-full h-[87px] flex justify-center items-center uppercase text-[15px] bg-center bg-no-repeat bg-[url(/img/case/category.png)] text-white">{cat}</b>
         <div class="flex flex-wrap w-full px-[8%]">
-        {#each filteredCases.filter(c => c.category == category) as c }
+        {#each showCases.filter(c => c.category == cat) as c }
             <Case {c} />
         {/each}
         </div>
